23dc12e6fd347ac7740cd7caf5be7c0c54210c9b
zhouxk <zhouxk@zgmmtuan.com>
2020-12-18 11:34:08 +0800

    update

--- src/view/facilitator/index.vue
+++ src/view/facilitator/index.vue
@@ -45,13 +45,7 @@ export default {
     testChildren, cDialog
   },
   created () {
-    console.log('revert-1')
-    console.log('revert-2')
-<<<<<<< HEAD
-    console.log('revert-3')
-    console.log('revert-4')
-=======
->>>>>>> parent of ac2e5ad8... revert-3
+
   },
   methods: {
     a (val) {
